95edaef96426e2fe05bbac577744e6714b9fa89e
Salim Karim <imsjk8@gmail.com>
2022-06-19 19:27:38 +1000

    fied modal data issue

--- pages/Components/Modal.js
+++ pages/Components/Modal.js
@@ -1,8 +1,14 @@
 import React from "react";
 import { useState, useContext } from "react";
 
-export default function Modal({ open, onClose, tokenAddress, tokenData, tokenName, tokenSymbol }) {
-
+export default function Modal({
+  open,
+  onClose,
+  tokenAddress,
+  tokenData,
+  tokenName,
+  tokenSymbol,
+}) {
   // const token = useContext(TokenContext);
 
   if (!open) {
@@ -14,34 +20,39 @@ export default function Modal({ open, onClose, tokenAddress, tokenData, tokenNam
         id="modal"
         className="bg-secondary text-primary max-w-sm p-3 shadow-lg fixed border-primary border-4 "
       >
-         <div
+        <div
           id="closeBtn"
           onClick={onClose}
           className="text-xl hover:text-secondary hover:bg-primary cursor-pointer w-min p-2 absolute right-1"
         >
           X
         </div>
-        <div className="font-karma text-bold text-4xl pb-10">TOKEN INFORMATION</div>
+        <div className="font-karma text-bold text-4xl pb-10">
+          TOKEN INFORMATION
+        </div>
 
         <p className="font-karma">Token Name</p>
-        <p className="text-2xl">{tokenName || 'got nothing'}</p>
+        <p className="text-2xl">{tokenName || "got nothing"}</p>
         <p className="font-karma">Token Symbol</p>
-        <p className="text-2xl">{tokenSymbol || 'got nothing'}</p>
+        <p className="text-2xl">{tokenSymbol || "got nothing"}</p>
         <h2 className="font-karma">Token Address</h2>
-        <p id="no" className="overflow-auto">{tokenAddress || 'got nothing'}</p>
+        <p id="no" className="overflow-auto">
+          {tokenAddress || "got nothing"}
+        </p>
         <button>✂️</button>
 
         <h2 className="font-karma">Total Supply</h2>
-        <p className="text-2xl">{(tokenData.result.value.amount / 1_000_000_000).toFixed(tokenData.result.value.decimals) || 'got nothing'}</p>
+        <p className="text-2xl">
+          {(tokenData.result.value.amount / 1_000_000_000).toFixed(
+            tokenData.result.value.decimals
+          ) || "got nothing"}
+        </p>
 
-        <a
-                        target="blank"
-                        href={`https://solscan.io/account/${tokenAddress}`}
-                      >
-                        {" "}
-                        🔦{" "}
-                      </a>
-       <div>{JSON.stringify(tokenData)}</div>
+        <a target="blank" href={`https://solscan.io/account/${tokenAddress}`}>
+          {" "}
+          🔦{" "}
+        </a>
+        <div>{JSON.stringify(tokenData)}</div>
       </div>
     </div>
   );
--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,7 @@ import { useState, createContext, useEffect, Fragment } from "react";
 import TwitterMeta from "./Components/TwitterMeta";
 import Image from "next/image";
 import Footer from "./Components/Footer";
+
 export default function Home({ tokens, count }) {
   const [searchTerm, setSearchTerm] = useState("");
   const [isOpen, setIsOpen] = useState(false);
@@ -14,8 +15,10 @@ export default function Home({ tokens, count }) {
   const [tokenData, setTokenData] = useState("");
   const [tokenName, setTokenName] = useState("");
   const [tokenSymbol, setTokenSymbol] = useState("");
-  const TokenContext = createContext("DhpikB5Qf4YZRzeGpFiAcZdSPAETS1mLF94PZz3oUos1");
- 
+  const TokenContext = createContext(
+    "DhpikB5Qf4YZRzeGpFiAcZdSPAETS1mLF94PZz3oUos1"
+  );
+
   useEffect(() => {
     let headersList = {
       Accept: "*/*",
@@ -40,14 +43,14 @@ export default function Home({ tokens, count }) {
       .then(function (data) {
         console.log(data);
         setTokenData(data);
-        setTokenName(tokenName);
-        setTokenSymbol(tokenSymbol);
       });
   }, [tokenAddress]);
 
-  function doStuff(tokenAddress) {
+  function doStuff(tokenAddress, tokenName, tokenSymbol) {
     setIsOpen(true);
     setTokenAddress(tokenAddress);
+    setTokenName(tokenName);
+    setTokenSymbol(tokenSymbol);
   }
 
   function clearSearch() {
@@ -56,157 +59,170 @@ export default function Home({ tokens, count }) {
     setSearchTerm("");
   }
   return (
-    <TokenContext.Provider>
+    <TokenContext.Provider value={tokenAddress}>
+      <div className={styles.container}>
+        <Head>
+          <title>Solana Tokens | tokenshit.xyz</title>
+          <TwitterMeta />
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
 
-    <div className={styles.container}>
-      <Head>
-        <title>Solana Tokens | tokenshit.xyz</title>
-        <TwitterMeta />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className="p-5 font-bold">
-        <div className="shadow-lg font-karma border-4 sticky top-0 bg-primary text-secondary backdrop-blur-lg p-5 text-3xl md:text-5xl text-center">
-          Solana Tokens
-          <div className="text-xl font-mono pt-3">Hit 🎯 or 💩 Shit</div>
-        </div>
-        <div className="text-center p-2 font-extralight italic">
-          There are currently {count} tokens registered.
-        </div>
-        <div>
-          <div className="grid grid-cols-[3fr_1fr]">
-            <input
-              onChange={(event) => {
-                setSearchTerm(event.target.value);
-              }}
-              className=" text-center bg-black text-white p-2 w-full md:3/4 lg:1/3 focus:border-primary-2 rounded-none focus:rounded-none focus:outline-none "
-              type="text"
-              id="searchBar"
-              placeholder={`Search here to filter...`}
-            ></input>
-            <button
-              className="inset-0 border-1 p-1 bg-primary hover:text-white"
-              value="Reset"
-              onClick={() => clearSearch()}
-            >
-              Clear
-            </button>
+        <main className="p-5 font-bold">
+          <div className="shadow-lg font-karma border-4 sticky top-0 bg-primary text-secondary backdrop-blur-lg p-5 text-3xl md:text-5xl text-center">
+            Solana Tokens
+            <div className="text-xl font-mono pt-3">Hit 🎯 or 💩 Shit</div>
           </div>
-        </div>
-        <div className={styles.grid}>
-          <>
-            {searchTerm && (
-              <>
-                <div className="text-2xl font-medium p-2">
-                  Found&nbsp;
-                  {
-                    tokens.filter((value) => {
-                      if (searchTerm === "") {
-                        return value;
-                      } else if (
-                        value.name
-                          .toLowerCase()
-                          .includes(searchTerm.toLowerCase())
-                      ) {
-                        return value;
-                      }
-                    }).length
-                  }
-                  &nbsp;tokens.
-                </div>
-              </>
-            )}
-            <div className="grid grid-cols-[2fr_2fr_4fr_4fr_1fr] gap-3 shadow-2xl p-5 mt-5">
-              <div className="header">Logo</div>
-              <div className="header">Symbol</div>
-              <div className="header">Name</div>
-              <div className="header">Links</div>
-              <div className="header">Deets</div>
-              {tokens
-                .filter((value) => {
-                  if (searchTerm === "") {
-                    return value;
-                  } else if (
-                    value.name.toLowerCase().includes(searchTerm.toLowerCase())
-                  ) {
-                    return value;
-                  }
-                })
-                .slice(0, 50)
-                .map((token) => (
-                  <Fragment key={token.address}>
-                    <div className="max-w-lg mx-auto">
-                      <img
-                        src={token.logoURI}
-                        alt={token.symbol}
-                        width="25"
-                        height="25"
-                      />
-                    </div>
-                    <div>{token.symbol}</div>
-                    <div>{token.name}</div>
-                    <div className="">
-                      {token.extensions?.website && (<a
-                        className=""
-                        href={
-                          token.extensions?.website + "?referrer=tokenshit.xyz"
+          <div className="text-center p-2 font-extralight italic">
+            There are currently {count} tokens registered.
+          </div>
+          <div>
+            <div className="grid grid-cols-[3fr_1fr]">
+              <input
+                onChange={(event) => {
+                  setSearchTerm(event.target.value);
+                }}
+                className=" text-center bg-black text-white p-2 w-full md:3/4 lg:1/3 focus:border-primary-2 rounded-none focus:rounded-none focus:outline-none "
+                type="text"
+                id="searchBar"
+                placeholder={`Search here to filter...`}
+              ></input>
+              <button
+                className="inset-0 border-1 p-1 bg-primary hover:text-white"
+                value="Reset"
+                onClick={() => clearSearch()}
+              >
+                Clear
+              </button>
+            </div>
+            {/* <div>address = {tokenAddress}</div>
+            <div>name = {tokenName}</div>
+            <div>symbol = {tokenSymbol}</div> */}
+          </div>
+          <div className={styles.grid}>
+            <>
+              {searchTerm && (
+                <>
+                  <div className="text-2xl font-medium p-2">
+                    Found&nbsp;
+                    {
+                      tokens.filter((value) => {
+                        if (searchTerm === "") {
+                          return value;
+                        } else if (
+                          value.name
+                            .toLowerCase()
+                            .includes(searchTerm.toLowerCase())
+                        ) {
+                          return value;
                         }
-                        target={"_blank"}
-                        rel="noreferrer"
-                      >
-                        <Image
-                          src={"/www.svg"}
-                          alt={token.extensions?.website}
-                          width={15}
-                          height={15}
+                      }).length
+                    }
+                    &nbsp;tokens.
+                  </div>
+                </>
+              )}
+              <div className="grid grid-cols-[1fr_2fr_4fr_1fr_1fr] gap-3 shadow-2xl p-5 mt-5">
+                <div className="header">Logo</div>
+                <div className="header">Symbol</div>
+                <div className="header">Name</div>
+                <div className="header">Links</div>
+                <div className="header">Deets</div>
+                {tokens
+                  .filter((value) => {
+                    if (searchTerm === "") {
+                      return value;
+                    } else if (
+                      value.name
+                        .toLowerCase()
+                        .includes(searchTerm.toLowerCase())
+                    ) {
+                      return value;
+                    }
+                  })
+                  .slice(0, 50)
+                  .map((token) => (
+                    <Fragment key={token.address} className="bg-primary col-span-5">
+                      <div className="max-w-lg mx-auto select-all ">
+                        <img
+                          src={token.logoURI}
+                          alt={token.symbol}
+                          width="25"
+                          height="25"
                         />
-                      </a>)}
-                      {token.extensions?.twitter && (
-                      <a
-                        className=""
-                        href={token.extensions?.twitter}
-                        target={"_blank"}
-                        rel="noreferrer"
-                      >
-                        <Image
-                          src={"/twitter.svg"}
-                          alt={token.extensions?.twitter}
-                          width={15}
-                          height={15}
-                        />
-                      </a>)}
-                      {token.extensions?.facebook && (
-                      <a
-                        className=""
-                        href={token.extensions?.facebook}
-                        target={"_blank"}
-                        rel="noreferrer"
-                      >
-                        <Image
-                          src={"/facebook.svg"}
-                          alt={token.extensions?.facebook}
-                          width={15}
-                          height={15}
-                        />
-                      </a>
-                      )}
-                      {token.extensions?.discord && (
-                      <a
-                        className="pl-1"
-                        href={token.extensions?.discord}
-                        target={"_blank"}
-                        rel="noreferrer"
-                      >
-                        <Image
-                          src={"/discord.svg"}
-                          alt={token.extensions?.discord}
-                          width={15}
-                          height={15}
-                        />
-                      </a>)}
-                    </div>
-                    <div>
-                      {/* <a
+                      </div>
+                      <div className="select-all selection:bg-primary/90 selection:text-white">
+                        {token.symbol}
+                      </div>
+                      <div className="select-all selection:bg-primary/90 selection:text-white">
+                        {token.name}
+                      </div>
+                      <div>
+                        {token.extensions?.website && (
+                          <a
+                            className=""
+                            href={
+                              token.extensions?.website +
+                              "?referrer=tokenshit.xyz"
+                            }
+                            target={"_blank"}
+                            rel="noreferrer"
+                          >
+                            <Image
+                              src={"/www.svg"}
+                              alt={token.extensions?.website}
+                              width={15}
+                              height={15}
+                            />
+                          </a>
+                        )}
+                        {token.extensions?.twitter && (
+                          <a
+                            className=""
+                            href={token.extensions?.twitter}
+                            target={"_blank"}
+                            rel="noreferrer"
+                          >
+                            <Image
+                              src={"/twitter.svg"}
+                              alt={token.extensions?.twitter}
+                              width={15}
+                              height={15}
+                            />
+                          </a>
+                        )}
+                        {token.extensions?.facebook && (
+                          <a
+                            className=""
+                            href={token.extensions?.facebook}
+                            target={"_blank"}
+                            rel="noreferrer"
+                          >
+                            <Image
+                              src={"/facebook.svg"}
+                              alt={token.extensions?.facebook}
+                              width={15}
+                              height={15}
+                            />
+                          </a>
+                        )}
+                        {token.extensions?.discord && (
+                          <a
+                            className="pl-1"
+                            href={token.extensions?.discord}
+                            target={"_blank"}
+                            rel="noreferrer"
+                          >
+                            <Image
+                              src={"/discord.svg"}
+                              alt={token.extensions?.discord}
+                              width={15}
+                              height={15}
+                            />
+                          </a>
+                        )}
+                      </div>
+                      <div>
+                        {/* <a
                         className="text-2xl cursor-pointer"
                         onClick={() => alert("Coming Soon")}
                       >
@@ -218,40 +234,42 @@ export default function Home({ tokens, count }) {
                       >
                         💩
                       </a> */}
-                      <a
-                        className="cursor-pointer"
-                        target="blank"
-                        onClick={() => doStuff(token.address)}
-                      >
-                        {" "}
-                        🔎{" "}
-                      </a>
-                      <a
-                        target="blank"
-                        href={`https://solscan.io/account/${token.address}`}
-                      >
-                        {" "}
-                        🔦{" "}
-                      </a>
-                    </div>
-                  </Fragment>
-                ))}
-            </div>
-          </>
-        </div>
-      </main>
-      <Footer />
-      <Modal
-        tokenAddress={tokenAddress}
-        tokenName={tokenName}
-        tokenSymbol={tokenSymbol}
-        tokenData={tokenData}
-        open={isOpen}
-        onClose={() => {
-          setIsOpen(false);
-        }}
-      />
-    </div>
+                        <a
+                          className="cursor-pointer"
+                          target="blank"
+                          onClick={() =>
+                            doStuff(token.address, token.name, token.symbol)
+                          }
+                        >
+                          {" "}
+                          🔎{" "}
+                        </a>
+                        <a
+                          target="blank"
+                          href={`https://solscan.io/account/${token.address}`}
+                        >
+                          {" "}
+                          🔦{" "}
+                        </a>
+                      </div>
+                    </Fragment>
+                  ))}
+              </div>
+            </>
+          </div>
+        </main>
+        <Footer />
+        <Modal
+          tokenAddress={tokenAddress}
+          tokenName={tokenName}
+          tokenSymbol={tokenSymbol}
+          tokenData={tokenData}
+          open={isOpen}
+          onClose={() => {
+            setIsOpen(false);
+          }}
+        />
+      </div>
     </TokenContext.Provider>
   );
 }
